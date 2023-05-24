ae08c1be309136564c0da61b5bfc627461767a76
NormanA1una <baciloferoz@gmail.com>
2023-05-24 16:49:01 -0600

    first commit spotiapp

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  corePlugins: {
+    preflight: false,
+  },
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
